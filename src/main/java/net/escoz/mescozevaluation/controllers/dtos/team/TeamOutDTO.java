bdda5bf6087a4c29535d241f31894ff2443ca938
Miguel Escoz <miguel.escoz@nter.es>
2024-12-04 12:54:04 +0100

    GETs para los equipos

--- src/main/java/net/escoz/mescozevaluation/controllers/dtos/team/TeamOutDTO.java
+++ src/main/java/net/escoz/mescozevaluation/controllers/dtos/team/TeamOutDTO.java
@@ -1,6 +1,5 @@
 package net.escoz.mescozevaluation.controllers.dtos.team;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
