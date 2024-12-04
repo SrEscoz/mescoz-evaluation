cbefafff7efd049a65510af83ab6bc041e9d5a27
Miguel Escoz <miguel.escoz@nter.es>
2024-12-04 08:22:15 +0100

    Commit inicial

---
+++ src/main/java/net/escoz/mescozevaluation/MescozEvaluationApplication.java
@@ -0,0 +1,13 @@
+package net.escoz.mescozevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MescozEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MescozEvaluationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/escoz/mescozevaluation/MescozEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.escoz.mescozevaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MescozEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
